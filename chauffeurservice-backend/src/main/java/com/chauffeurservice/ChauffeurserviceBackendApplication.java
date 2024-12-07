cab264f4ce67f8803a0e823006009212e2aa8102
Furkan Selvi <furkanselvi1996@icloud.com>
2024-12-07 17:00:26 +0300

    init project-backend

---
+++ chauffeurservice-backend/src/main/java/com/chauffeurservice/ChauffeurserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chauffeurservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChauffeurserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChauffeurserviceBackendApplication.class, args);
+	}
+
+}
