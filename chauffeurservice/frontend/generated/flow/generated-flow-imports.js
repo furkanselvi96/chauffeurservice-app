8024bfa5faa3adc5a48cb660fee2bbef316ee413
Furkan Selvi <furkanselvi1996@icloud.com>
2024-12-22 01:00:40 +0300

    Projeye Vaadin framework eklendi.

---
+++ chauffeurservice/frontend/generated/flow/generated-flow-imports.js
@@ -0,0 +1,20 @@
+import '@vaadin/common-frontend/ConnectionIndicator.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vaadin-lumo-styles/color-global.js';
+import '@vaadin/vaadin-lumo-styles/typography-global.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
+
+const loadOnDemand = (key) => {
+  const pending = [];
+  if (key === 'ac7ca9d317a9d80e2ff8380edc897f6e1bd9fa6ae6da6970121f029b87cbf5e5') {
+    pending.push(import('./chunks/chunk-d92a06a7762515ac5eda73ee8e6b3de6dd7322906dce88c06ef0f1a15023e386.js'));
+  }
+  return Promise.all(pending);
+}
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.Flow = window.Vaadin.Flow || {};
+window.Vaadin.Flow.loadOnDemand = loadOnDemand;
